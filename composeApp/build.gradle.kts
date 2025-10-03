75192f56885403244eaebf3446ded0cfcfba3ca5
hiroaki404 <nosho404@gmail.com>
2025-10-03 23:02:29 +0900

    Add koog agents dependency and create initial Kotlin notebook

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -32,6 +32,7 @@ kotlin {
             implementation(compose.components.uiToolingPreview)
             implementation(libs.androidx.lifecycle.viewmodelCompose)
             implementation(libs.androidx.lifecycle.runtimeCompose)
+            implementation(libs.koog.agents)
         }
         commonTest.dependencies {
             implementation(libs.kotlin.test)
